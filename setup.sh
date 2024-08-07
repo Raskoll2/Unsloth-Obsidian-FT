53083f7aaedfdfa8a01bf3f8587154c32017e694
Raskoll <141559281+Raskoll2@users.noreply.github.com>
2024-08-08 00:38:23 +1000

    fixed dependancies

--- setup.sh
+++ setup.sh
@@ -14,10 +14,12 @@ pip install transformers datasets dropbox
 # Handle Google Colab specific setup
 if [ "$1" == "--colab" ]; then
   echo "Running in Google Colab. Installing Unsloth with Colab support..."
-  pip install "unsloth[colab] @ git+https://github.com/unslothai/unsloth.git" --quiet
+  pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
+  pip install --no-deps "xformers<0.0.27" "trl<0.9.0" peft accelerate bitsandbytes
 else
   echo "Installing Unsloth..."
   pip install unsloth
+  pip install peft accelerate bitsandbytes xformers trl
 fi
 
 # Run the Python script
